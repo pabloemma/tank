b231b5945b47d83d08bb436e84b70a4ee5d6f0e1
andi klein <pabloemma@gmail.com>
2018-01-29 07:44:44 -0700

    pushes tank data to server.first commit

---
+++ tank/src/data_push.py
@@ -0,0 +1,25 @@
+'''
+Created on Jan 29, 2018
+will push the tank data
+
+@author: klein
+'''
+
+import socket
+import sys
+from multiprocessing.connection import Client
+class Push(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+
+
+if __name__ == '__main__':
+    pass
