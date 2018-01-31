b04a0b98b411f921631bd1c51af1e29facdea291
andi klein <pabloemma@gmail.com>
2018-01-30 18:52:41 -0700

    first commit of the level read

---
+++ tank/src/Level.py
@@ -0,0 +1,39 @@
+'''
+Created on Jan 29, 2018
+
+@author: klein
+'''
+import time
+import serial
+
+class MyLevel(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, device):
+        '''
+        Constructor
+        '''
+        self.ser = serial.Serial(
+            port = device,
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=0)
+
+        counter=0
+    
+    def Measure(self):
+        x=self.ser.readline()
+        print x 
+        time.sleep(1)
+
+ 
+if __name__ == '__main__':
+    device_name = '/dev/ttyAMA0'
+    lev = MyLevel(device_name)
+    lev.Measure()
+        
