101df11a49b81d549ab497754784e27920fe7090
andi klein <pabloemma@gmail.com>
2018-01-30 19:00:39 -0700

    main program to read tank level

---
+++ tank/src/tanklevel.py
@@ -0,0 +1,26 @@
+'''
+Created on Jan 30, 2018
+
+@author: klein
+'''
+import Level as Lv
+import data_push as dp
+if __name__ == '__main__':
+    #establish connection
+    ip_server = '192.168.2.9' # change for apporpitae server
+    server_port = 5478
+    # serial connection device
+    device_name = '/dev/ttyAMA0'
+
+    
+    MyPush = dp(ip_server,server_port)
+    MyPush.Connect2Server()
+    # now open up the serial port
+    lev = Lv(device_name)
+    data = lev.Measure()
+
+    MyPush.PushData(data)
+    MyPush.CloseConnection()    
+    
+    
+    
