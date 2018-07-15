3333de96d516d417c17d113231cbd01a72a9fb5d
andi klein <pabloemma@gmail.com>
2018-07-15 10:01:18 -0600

    more changes to plot

--- tank/src/plot_tank_level.py
+++ tank/src/plot_tank_level.py
@@ -8,6 +8,9 @@ import time
 import os
 import datetime
 
+import matplotlib.pyplot as plt
+import csv
+
 
 
 
@@ -28,12 +31,37 @@ class plot_tank_level(object):
         self.x = 0
         self.y = 0
         self.z = 0
+        self.filedate = filedate
 
 
         def ReadFile(self):
+            
+            
+            a = datetime.datetime.today().strftime('%Y-%m-%d')
+            filename = a+'tank.csv'
+            # if filename exists we open in append mode
+            #otherwise we will create it
+            homedir = os.environ['HOME']
+            filename = homedir + '/tankfiles/'+filename
+            print filename
+            if os.path.isfile(filename):
+                self.input = open(filename,'r',0)
+            else :
+                print "cannot find file"
+                sys.exit()
+
+            
             with open('/home/klein/tankfiles/2018-07-13tank.csv','r') as csvfile:
                 plots = csv.reader(csvfile, delimiter=',')
                 for row in plots:
-                    x.append(int(row[0]))
-                    y.append(int(row[1]))
-                    z.append(int(row[1]))
+                    self.x.append(int(row[0]))
+                    self.y.append(int(row[1]))
+                    self.z.append(int(row[2]))
+                    
+    def PlotLevel(self):
+        plt.plot(self.x,self.z, label='levels in tank')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Irrigation System')
+        plt.legend()
+        plt.show()
