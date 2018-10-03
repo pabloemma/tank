b7b19f832ac3e0f0a2025b0da54b5542943df638
andi klein <klein@casitadongaspar.com>
2018-10-03 07:44:10 -0600

    working date axis

--- tank/src/plot_level.py
+++ tank/src/plot_level.py
@@ -3,14 +3,15 @@ import matplotlib.dates as md
 import csv
 import time
 import sys
-import datetime
+import datetime as dt
+import numpy as np
 #from scipy.special._mptestutils import Arg
 
 # check for arguments
 for arg in sys.argv[1:]:
     print arg
     if (arg == 'today'):
-        date_string=datetime.datetime.today().strftime('%Y-%m-%d')
+        date_string=dt.datetime.today().strftime('%Y-%m-%d')
     elif("-" in arg): date_string = arg
         
     else:
@@ -47,13 +48,43 @@ plt.title('Plot of tanklevels ')
 plt.legend()
 plt.show()
 
+
 # now do second way of plotting
 plt.figure(2)
+
+time_date1 = [dt.datetime.fromtimestamp(ts) for ts in x1]
+time_date2 = [dt.datetime.fromtimestamp(ts) for ts in x2]
+
+time_convert1 = md.date2num(time_date1)
+time_convert2 = md.date2num(time_date2)
+#print time_date
+
+
 plt.subplot(211)
-plt.plot(x1,y1, 'g^',label='Loaded from file!')
+plt.xticks(rotation = 25)
+ax=plt.gca()
+#xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+xfmt = md.DateFormatter(' %H:%M:%S')
+ax.xaxis.set_major_formatter(xfmt)
+ax.xaxis_date()
+#plt.gcf().autofmt_xdate()
+
+#plt.locator_params(axis='x',nbins =5)
+plt.plot(time_date1,y1, 'g^',label='Loaded from file!')
 
 plt.subplot(212)
-plt.plot(x2,y2,"r.") # plot with red points
+plt.subplots_adjust(bottom = .1)
+plt.xticks(rotation = 25)
+ax=plt.gca()
+#xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+xfmt = md.DateFormatter(' %H:%M:%S')
+ax.xaxis.set_major_formatter(xfmt)
+ax.xaxis_date()
+#plt.gcf().autofmt_xdate()
+
+#plt.locator_params(axis='x',nbins =5)
+plt.plot(time_date2,y2,"r.") # plot with red points
 plt.grid(True)
+plt.subplots_adjust(hspace=.3)
 plt.show()
 
